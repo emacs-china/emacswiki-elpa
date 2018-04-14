d1a8cf1be754d79e7dd2f8bdf55b5a881a87f668
Xu Chunyang <mail@xuchunyang.me>
2018-04-14 23:02:50 +0800

    Revert "Update epkg.sqlite every time"
    This reverts commit 0d8be0c84f18a65e14f452dabf197e39d80dab2a.

--- emacswiki-elpa.el
+++ emacswiki-elpa.el
@@ -32,7 +32,8 @@
 (defun emacswiki-elpa-recipe-alist ()
   (let (list)
     (let ((db (expand-file-name "epkg.sqlite" emacswiki-elpa-pwd)))
-      (url-copy-file "https://raw.githubusercontent.com/emacsmirror/epkgs/master/epkg.sqlite" db 'ok-if-already-exists)
+      (unless (file-exists-p db)
+        (url-copy-file "https://raw.githubusercontent.com/emacsmirror/epkgs/master/epkg.sqlite" db))
       (with-temp-buffer
         (call-process "sqlite3" nil t nil
                       db "-separator" " " "select name, mirror_url from packages where class='wiki'")
